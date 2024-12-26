80bb4eb1b500ff73910ed94477c6890bf3282fb4
zhangjunpeng <zhangjunpeng@thinredline@com.cn>
2024-12-26 19:02:16 +0800

    安装依赖和配置文件

---
+++ .eslintrc.cjs
@@ -0,0 +1,53 @@
+// .eslintrc.js
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: "./tsconfig.json",
+  },
+  plugins: ["@typescript-eslint", "react", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "react/prop-types": "off",
+    "prettier/prettier": "error",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
+
+// module.exports = {
+//   root: true,
+//   env: { browser: true, es2020: true, serviceworker: true },
+//   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
+//   ignorePatterns: [
+//     'dist',
+//     '.eslintrc.cjs',
+//     'tailwind.config.cjs',
+//     'postcss.config.cjs',
+//     'tsconfig.json',
+//     'tsconfig.node.json',
+//     'forge.config.cjs',
+//     'plugin.cjs',
+//     'electron-builder.config.js',
+//     'wasm_exec.js',
+//     'scripts',
+//   ],
+//   parser: '@typescript-eslint/parser',
+//   plugins: ['react-refresh'],
+//   rules: {
+//     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+//   },
+// };
