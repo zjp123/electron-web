a9155422f2c03a7c33d536f5659fe1a559366205
zhangjunpeng <zhangjunpeng@thinredline@com.cn>
2024-12-27 11:06:56 +0800

    feat: 解决ci问题

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -6,4 +6,8 @@ module.exports = {
   //   "stylelint --cache --fix", // 使用 Stylelint 修复 CSS/SCSS 和 styled-components 中的样式
   // ],
   "*.{json,md}": ["prettier --write"],
+  // 忽略以下文件或目录
+  "dist/**": "",
+  "node_modules/**": "",
+  "coverage/**": "",
 };
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,17 @@
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
-};
+// module.exports = {
+//   extends: ["@commitlint/config-conventional"],
+// };
 
 // export default {
 //   extends: ["@commitlint/config-conventional"],
 // };
+
+const Configuration = {
+  /*
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
+  extends: ["@commitlint/config-conventional"],
+};
+
+export default Configuration;
