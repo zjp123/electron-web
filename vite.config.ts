8be768d7bb362d4ef121124fd1cf1022c60a4558
zhangjunpeng <zhangjunpeng@thinredline@com.cn>
2024-12-27 10:53:47 +0800

    feat: 解决ci问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 // import path from "path";
-import tsconfigPaths from "vite-tsconfig-paths";
+// import tsconfigPaths from "vite-tsconfig-paths";
 import postcssPresetEnv from "postcss-preset-env";
 
 export default defineConfig(() => {
@@ -9,7 +9,7 @@ export default defineConfig(() => {
 
   return {
     envDir: "./",
-    plugins: [react(), tsconfigPaths()],
+    plugins: [react()],
     // resolve: {
     //   alias: {
     //     "@": path.resolve(__dirname, "./src"), // 配置路径别名
