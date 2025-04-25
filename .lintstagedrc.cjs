7b7943826bec8684c209a4240e648d92bfc521cf
ext.zhangjunpeng5 <ext.zhangjunpeng5@jd.com>
2025-04-25 17:10:38 +0800

    feat: 增加h5项目

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
     "prettier --write", // 使用 Prettier 格式化 JavaScript/TypeScript 文件
   ],
   "*.{css,scss,less}": [
-    // "stylelint --cache --fix", // 使用 Stylelint 修复 CSS/SCSS 和 styled-components 中的样式
+    "stylelint --cache --fix", // 使用 Stylelint 修复 CSS/SCSS 和 styled-components 中的样式
     "prettier --write", // 使用 Prettier 格式化样式文件
   ],
   "*.{json,md}": ["prettier --write"],
